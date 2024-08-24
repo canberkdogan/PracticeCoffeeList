048f02a7462464171ebfda37d9da431f4983ac8d
canbe <canbe@CANBERK>
2024-08-25 01:24:28 +0300

    Add project files.

---
+++ PracticeCoffeeList/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+List<string> coffeeList = new List<string>();
+Console.WriteLine("Please enter 5 coffe names--->");
+
+for (int i = 0; i < 5 ; i++)
+{
+    Console.WriteLine($"Coffe {i + 1}");
+    string coffeeName = Console.ReadLine();  
+    coffeeList.Add( coffeeName );
+}
+
+Console.WriteLine("----------HERE IS YOUR COFFEE--------");
+
+foreach (string coffee in coffeeList)
+{
+
+    Console.WriteLine(coffee);
+}   
+
+
+
